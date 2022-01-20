d42d7f0c4575f618ad429618c226d1a6e10bf2c6
MaliekS <95492003+MaliekS@users.noreply.github.com>
2022-01-20 15:53:43 +0000

    Initial commit

---
+++ goat.py
@@ -0,0 +1,5 @@
+class Goat:
+  def __init__(self,_color,_maxSpeed,_min):
+      self.color=_color
+      self.maxspeed=_maxSpeed
+      self.minSpeed=_min
---
+++ main.py
@@ -0,0 +1,5 @@
+class Lion:
+  def __init__(self,_color,_maxSpeed,_voice range):
+self.color= _color
+self._maxSpeed=_maxSpeed
+self   
